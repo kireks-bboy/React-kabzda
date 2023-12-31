899e989d73624655a546bec77864e94fbfa4ed3b
Kirill Denisevitch <denviko00@gmail.com>
2023-12-31 20:44:08 +0300

    start

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <div className='header'>
+     <img src="https://розывашеймечты.рф/wp-content/uploads/2018/03/VK.com-logo.svg_.png" alt="" />
+    </div>
+  );
+}
+
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+function Profile() {
+  return (
+    <div className='profile'>
+      
+      <div>
+      <img src="https://www.lebholidays.com/images/charters/4/4-cabin_charter-02.jpg" alt="" />
+      </div>
+
+
+      <div>
+        <img src="  https://geek2u.ru/wp-content/uploads/2018/11/Kak-sdelat-ssylku-na-cheloveka-v-VK-1024x1024.jpg" alt="" />
+        ava + descr</div>
+    <div>
+      My posts
+      <div>New post</div>
+      <div>
+        post 1
+      </div>
+      <div>
+        post 2
+      </div>
+    </div>
+
+ 
+    </div>
+  );
+}
+
+
+export default Profile
